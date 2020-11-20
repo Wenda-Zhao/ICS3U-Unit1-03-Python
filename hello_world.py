d941b51a759395a7fb6f3fc552cc1c6ffc9408cf
Ubuntu <ubuntu@ip-172-31-84-246.ec2.internal>
2020-11-20 20:08:08 +0000

    added in hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+   
+# Created by Wenda Zhao
+# Created on Nov 2020
+# This is the "Hell, World!" program, with proper style
+   
+   
+def main():
+    print("Hello, World! 你好，世界！")
+       
+       
+if __name__ == "__main__":
+       main()
